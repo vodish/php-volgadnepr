c2b8587e60e63a35864c61d917de114108291a8b
pkarasev <p.karasev@psw.ru>
2024-03-29 15:58:05 +0300

    upd

--- app/Http/Controllers/AircraftAirportsController.php
+++ app/Http/Controllers/AircraftAirportsController.php
@@ -5,25 +5,43 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-
 class AircraftAirportsController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * ручка    /api/aircraft_airports
      */
     public function index()
     {
-        //
-        $select = DB::select("
+
+        # параметры
+        #
+        $tail       =   'TEST-001' ;    // - бортовой номер воздушного судна
+        $date_from  =   '';             // - начало периода (формат: yyyy-mm-dd hh:mm )     flights.takeoff
+        $date_to    =   '';             // - конец периода (формат: yyyy-mm-dd hh:mm )      flights.landing
+
+
+
+        # получить данные из базы
+        #
+        $select     = DB::select("
             SELECT
-                *
+                 flights.*
+                , airfrom.code_iata     as  airfrom_code_iata
+                , airto.code_iata       as  airto_code_iata
             FROM
-                airports
+                flights
+                    INNER JOIN  aircrafts               ON  flights.aircraft_id = aircrafts.id
+                    INNER JOIN  airports as airfrom     ON  flights.airport_id1 = airfrom.id
+                    INNER JOIN  airports as airto       ON  flights.airport_id2 = airto.id
+            WHERE
+                aircrafts.tail = '$tail'
             LIMIT
                 1
         ");
 
-        dd($select);
+        // dd($select);
+        
+        return response()->json($select);
     }
 
     /**
